6033bbed6d3621bcdfb1100287ca8acb9c9f2fe4
Viacheslav Strogalev <strogalev@hotmail.com>
2024-05-26 09:29:28 +0300

    Added id for playlists and tracks

--- index.js
+++ index.js
@@ -29,11 +29,13 @@ console.log(htmlElementSelectTechnologies)
 const playlists = [
 	{
 		playlistInfo: {
+      id: '1-0',
 			playlistCoverImage: "./img/cardImage/image1.jpeg",
 			title: "Hip-Hop Hits"
 		},
 		tracks: [
 			{
+        id: '1',
 				artistName: "Eminem",
 				trackTitle: "Rap God",
 				trackCoverImage: "./img/cardImage/trackList/track1.jpeg",
@@ -42,6 +44,7 @@ const playlists = [
 				duration: 221,
 			},
 			{
+        id: '2',
 				artistName: "50 cent",
 				trackTitle: "In da Club",
 				trackCoverImage: "./img/cardImage/trackList/track1.jpeg",
@@ -53,11 +56,13 @@ const playlists = [
 	},
 	{
     playlistInfo: {
+      id: '1-1',
       playlistCoverImage: "./img/cardImage/image2.jpeg",
       title: "Rap Hits 1990s"
     },
 		tracks: [
 			{
+        id: '3',
 				artistName: "Public Enemy",
 				trackTitle: "Fight the Power",
 				trackCoverImage: "./img/cardImage/trackList/track5.jpeg",
@@ -66,6 +71,7 @@ const playlists = [
 				duration: 282,
 			},
 			{
+        id: '4',
 				artistName: "Vanila Ice",
 				trackTitle: "Ice Ice Baby",
 				trackCoverImage: "./img/cardImage/trackList/track6.jpeg",
